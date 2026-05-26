a2be0dbe4e9b42d85e0e01ce14a6e09a5e425d9e
yadavpavangit <yadavp07.07@gmail.com>
2026-05-26 15:12:26 +0530

    hide navbar on dashbaord, change backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,20 @@ import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
+  const location = useLocation();
+  const hideNavbar = location.pathname.startsWith("/myAdmin") ||
+    location.pathname.startsWith("/product-create") ||
+    location.pathname.startsWith("/all-products");
+
   return (
     <>
-      <Navbar />
+      {!hideNavbar && <Navbar />}
       <main className="w-full bg-black">
         <Outlet />
       </main>
